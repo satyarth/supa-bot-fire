4f3506b54d15105ecf34b6b1ba0c36103f59b7c6
satyarth.mishra@gmail.com <satyarth.mishra@gmail.com>
2015-06-01 04:36:24 +0100

    Handle sentences ending in punctuation

--- supabotfire.py
+++ supabotfire.py
@@ -6,6 +6,7 @@ from tweepy import API
 from tweepy import error as tweepyerror
 from ConfigParser import SafeConfigParser
 from json import loads
+import HTMLParser
 import nltk
 
 # Read API credentials from config file
@@ -18,7 +19,7 @@ CONSUMER_SECRET = parser.get('twitter', 'consumer.secret')
 ACCESS_KEY = parser.get('twitter', 'access.key')
 ACCESS_SECRET = parser.get('twitter', 'access.secret')
 
-disallowed_verbs = ["\'m", "am", "\'ve", "wan"]
+disallowed_verbs = ["\'m", "am", "\'ve", "wan", "cant"]
 verb_forms = ["VB", "VBD", "VBP"]
 banned_strings = ["Facebook", "YouTube", "http://", "https://", "www.", ".com"]
 
@@ -35,6 +36,8 @@ class StdOutListener(StreamListener):
         try:
             parsed_data = loads(data)
             text = parsed_data['text']
+            h = HTMLParser.HTMLParser()
+            text = h.unescape(text)
             status_id = parsed_data['id']
             if any(banned_string in text for banned_string in banned_strings):
                 pass
@@ -42,13 +45,20 @@ class StdOutListener(StreamListener):
                 try:
                     text = sentence_detector.tokenize(text.strip())[0]
                     tag_list = nltk.pos_tag(nltk.tokenize.word_tokenize(text))
-                    if tag_list[0][1] == 'PRP'and tag_list[1][1] in verb_forms and not tag_list[1][0].lower() in disallowed_verbs and not tag_list[2][0] in ["n\'t"] and not any("CC" == tag[1] for tag in tag_list):
-                        message += "@"+parsed_data['user']['screen_name'] + " "
-                        for tag in tag_list[2:-2]:
+                    if tag_list[0][1] == 'PRP' \
+                    and tag_list[1][1] in verb_forms \
+                    and not tag_list[1][0].lower() in disallowed_verbs \
+                    and not tag_list[2][0] in ["n\'t"] \
+                    and not any("CC" == tag[1] for tag in tag_list):
+                        for tag in tag_list[2:-1]:
                             message += tag[0] + " "
-                        message += tag_list[-1][0]
+                        if tag_list[-1][0] not in [',', '.', '!', '?']:
+                            message += tag_list[-1][0]
                         message += ": " + tag_list[0][0] + " " + tag_list[1][0] + " that."
+                        message += " @"+parsed_data['user']['screen_name']
+                        print text
                         print message
+                        print tag_list
                         try:
                             api.update_status(status=message, in_reply_to_status_id=status_id)
                         except tweepyerror.TweepError:
